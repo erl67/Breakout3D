4eb3a60320f25f519e75f1ba20aa8e8c712b7929
Yanbo Wang <1995wangyanbo@gmail.com>
2018-04-19 23:01:35 -0400

    Exciting new updates!!
    1. All physics now functioning well!
    2. Refined pad movement. Although it can only controlled by mouse now, but the feeling of control is better!
    3. Ball becomes a prefab now, and it can instantiate right above where the pad is by clicking left mouse button.
    4. On launching the game, press anykey to start the game so you can move the panel. After finding a good spot, click the left mouse button to launch the ball.
    
    Problems:
    1. Once I make the ball as a prefab, the text object loses its attachment to the ball. Therefore, it stops the code from moving forward. I have to comment it in the script of the ball.
    2. For the same reason, maybe we need to find a better way to fix the score system.
    
    Next step:
    1. Designing levels, maybe add something like bonus to make this game more fun.
    2. Add arts to make this game looks better.

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -10,6 +10,8 @@ public class BallController : MonoBehaviour {
     private Vector3 force;
     private GameObject player;
     private Renderer mr;
+	private float constantSpeed = 30f;
+	//private Vector3 startSpeed;
     public Text txtScore, txtLives;
 
     public AudioSource[] sounds;
@@ -24,7 +26,7 @@ public class BallController : MonoBehaviour {
         switch (SceneManager.GetActiveScene().buildIndex)
         {
             case 0:
-                ballScale = 4f;
+                ballScale = 3f;
                 playerForce = 3f;
                 playerRotation = 1f;
                 blockForce = 50f;
@@ -39,8 +41,8 @@ public class BallController : MonoBehaviour {
                 break;
         }
         ball = this.gameObject;
-        ball.transform.localScale = new Vector3(ballScale * Random.Range(.5f, 1f), ballScale * Random.Range(.5f, 1f), ballScale * Random.Range(.5f, 1f));
-        //ball.transform.localScale *= ballScale;
+        //ball.transform.localScale = new Vector3(ballScale * Random.Range(.5f, 1f), ballScale * Random.Range(.5f, 1f), ballScale * Random.Range(.5f, 1f));
+        ball.transform.localScale *= ballScale;
 
         rb = ball.GetComponent<Rigidbody>();
         mr = ball.GetComponent<Renderer>();
@@ -50,86 +52,100 @@ public class BallController : MonoBehaviour {
         sounds = GetComponents<AudioSource>();
         //blockHit = null;
         //blockHit = sounds[0];
+		force = new Vector3(2f, 10f, 0f) * 5f;
+		rb.AddForce(force, ForceMode.Impulse);
+
     }
 
-    void Update () {
+	void Update () //I commented the lines that restrict the ball from moving along z axis, but did the same thing in the Unity by checking "Freeze the position (z)"
+	{
         //if (transform.position.z != 0f) transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
-        if (transform.position.z > Mathf.Abs(.5f)) transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
-
+        //if (transform.position.z > Mathf.Abs(.5f)) transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
+		//if (transform.position.z != 0f) transform.position.z = 0f;
+		rb.velocity  = constantSpeed * (rb.velocity.normalized);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
-        Debug.Log("BC with " + other.tag + " @ " + gameObject.transform.position);
+		//Debug.Log("BC with " + other.collider.tag + " @ " + gameObject.transform.position);
 
-        if (other.tag.Equals("block"))
-        {
-            int points = (int) other.GetComponent<Rigidbody>().mass;
-            Debug.Log("block died : " + gameObject.transform.position + " " + points);
+		if (other.gameObject.tag.Equals("block"))
+		{ 
+			//!!!!I commented the next line. Because once I make the ball as a prefab, the text lose its attachment to the ball. Therefore, it stops the code from moving forward.
+			//int points = (int) other.gameObject.GetComponent<Rigidbody>().mass;
+
+
+            //Debug.Log("block died : " + gameObject.transform.position + " " + points);
             blockHit.Play();
             //int points = (int) other.GetComponent<Rigidbody>().mass;
             //blockHit.Play();
             //mr.material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
             //mr.material.color = other.GetComponent<Renderer>().material.color * .5f;
             //mr.material.color -= other.GetComponent<Renderer>().material.color;
-            mr.material.color *= other.GetComponent<Renderer>().material.color;
+            mr.material.color *= other.gameObject.GetComponent<Renderer>().material.color;
 
 
-            force = new Vector3(Random.Range(-1 * blockForce, blockForce), 0f, 0) * 2f;
+            //force = new Vector3(Random.Range(-1 * blockForce, blockForce), 0f, 0) * 2f;
             //force = new Vector3(blockForce, 0f, 0) * 2f; //need to find and set magnitude from edge
 
-            rb.AddForce(force, ForceMode.Acceleration);
+            //rb.AddForce(force, ForceMode.Acceleration);
 
-            rb.velocity *= .9f;
+            //rb.velocity *= .9f;
             //rb.angularVelocity = rb.angularVelocity * Random.Range(-1 * blockRotation, blockRotation);
+			rb.velocity  = constantSpeed * (rb.velocity.normalized);
 
-            GameController.score += points;
-            txtScore.text = "Score: " + GameController.score;
+			//!!!!So do the next two lines as well
+            //GameController.score += points;
+            //txtScore.text = "Score: " + GameController.score;
+
 
 			//blockDied.Play ();
             Destroy(other.gameObject);
         }
 
-        if (other.tag.Equals("player"))
+        if (other.gameObject.tag.Equals("player"))
         {
 			bounce.Play ();
-            force = new Vector3(0f, 10f, 0f) * 3f;
-            rb.AddForce(force, ForceMode.Impulse);
-            oppositeForce = player.GetComponent<Rigidbody>().velocity;
-            Debug.Log("player velocity: " + oppositeForce + "  ball velocity: " + rb.velocity);
+            //force = new Vector3(0f, 10f, 0f) * 3f;
+            //rb.AddForce(force, ForceMode.Impulse);
+            //oppositeForce = player.GetComponent<Rigidbody>().velocity;
+            //Debug.Log("player velocity: " + oppositeForce + "  ball velocity: " + rb.velocity);
 
-            rb.velocity = Vector3.zero;
-            rb.angularVelocity = Vector3.zero;
+            //rb.velocity = Vector3.zero;
+            //rb.angularVelocity = Vector3.zero;
 
-            force = new Vector3(0f, 10f, 0f) * playerForce;
-            rb.AddForce(force, ForceMode.Impulse);
-
+            //force = new Vector3(0f, 10f, 0f) * playerForce;
+            //rb.AddForce(force, ForceMode.Impulse);
+			rb.velocity  = constantSpeed * (rb.velocity.normalized);
             GameController.score++;
             txtScore.text = "Score: " + GameController.score;
         }
 
-        if (other.tag.Equals("edge"))
+        if (other.gameObject.tag.Equals("edge"))
         {
+			rb.velocity  = constantSpeed * (rb.velocity.normalized);
             GameController.score += -1;
             txtScore.text = "Score: " + GameController.score;
         }
 
-        if (other.tag.Equals("top"))
+        if (other.gameObject.tag.Equals("top"))
         {
-            rb.velocity = Vector3.zero;
-            rb.angularVelocity = Vector3.zero;
+			rb.velocity  = constantSpeed * (rb.velocity.normalized);
+            //rb.velocity = Vector3.zero;
+            //rb.angularVelocity = Vector3.zero;
         }
 
-        if (other.tag.Equals("bottom"))
+        if (other.gameObject.tag.Equals("bottom"))
         {
-            mr.material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-
-            rb.transform.localScale *= .5f;
-            rb.velocity = Vector3.zero;
-            rb.angularVelocity = Vector3.zero;
-            rb.useGravity = false;
+            //mr.material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+			Destroy(gameObject);
+            //rb.transform.localScale *= .5f;
+            //rb.velocity = Vector3.zero;
+            //rb.angularVelocity = Vector3.zero;
+            //rb.useGravity = false;
             //rb.isKinematic = true;
         }
+		rb.velocity  = constantSpeed * (rb.velocity.normalized);
     }
 
     private void OnBecameInvisible()
